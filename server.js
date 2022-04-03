4f02372e770fbb3a62e4faddb9933f73c3b4332a
Sol-Snow <solsnow.ss@gmail.com>
2022-04-03 14:49:53 +0900

    EJS 사용, server.js
    Added EJS package.
    Image upload working.

--- server.js
+++ server.js
@@ -1,34 +1,79 @@
 // app.js
-const express = require('express')
-const multer = require('multer')
-const path = require('path')
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+//const ejs = require('ejs');
+
+//Multer storage
+const imageStorage = multer.diskStorage({
+    //Directory
+    destination: './images/',
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+//Multer image option
+const imageUpload = multer({
+    storage: imageStorage,
+    //Limit = 10 MB
+    limits: {fileSize: 10000000},
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).single('myImage');
+
+// Check File Type
+function checkFileType(file, cb){
+    // Allowed extentions.
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check extention.
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+
+    if(mimetype && extname){
+        return cb(null,true);
+    } else {
+        cb('Error: Images Only!');
+    }
+}
+
 const app = express();
 
-//multer option
-const upload = multer({
-    //dest: 'images',
-    limits: {fileSize: 10000000},
-    image: {type: Buffer},
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
-            cb(new Error('Please upload an image.'))
-        } cb (undefined, true)
-    }
-})
+const port = 5000;
 
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+app.use(express.static('./images'));
+//app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+//app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+//app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+
+app.get('/', (req, res) => res.render('index'));
+
+//Single image upload
+app.post('/upload', (req, res) => {
+    imageUpload(req, res, (err) => {
+        if(err){
+            res.render('index', {
+                msg: err
+            });
+        } else {
+            if(req.file == undefined){
+                res.render('index', {
+                    msg: 'Error: No File Selected!'
+                });
+            } else {
+                res.render('index', {
+                    msg: 'File Uploaded!',
+                    file: `uploads/${req.file.filename}`
+                });
+            }
+        }
+    });
 });
 
-app.get('/upload', (req, res) => {
-    res.sendFile(path.join(__dirname, 'upload.html'))
-});
-
-
-app.listen(5000, () => {
-    console.log('Listening on port ' + 5000);
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
 });
