42758b4f6b4720971381695035822fcb46470ff4
Sol-Snow <solsnow.ss@gmail.com>
2022-04-23 23:32:08 +0900

    imageUpload.js, user.js 추가

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const path = require('path');
 const express = require('express');
+/*const Joi = require('joi');
+Joi.objectID = require('joi-objectid')(Joi);*/
+const mongoose = require('mongoose');
+
+const imageUploadRouter = require("./backend/routes/imageUpload")
+const usersRouter = require("./backend/routes/users")
+
 const app = express();
 const port = 5000;
 
-const imageUploadRouter = require("./backend/routes/imageUpload")
-//const usersRouter = require("./backend/routes/users")
+//Connect to MongoDB
+const dbURI = 'mongodb+srv://younghunkim1:gung-on-password@gung-on-account-db.2z0gd.mongodb.net/Gung-on_Account_Database?retryWrites=true&w=majority'
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true})
+    .then((result) => console.log("MongoDB connection established."))
+    .catch((err) => console.log(err));
 
 app.use(express.static(__dirname + '/frontend'));
+app.use(express.json());
+app.use(usersRouter);
 app.use(imageUploadRouter);
-//app.use(usersRouter);
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/frontend/login.html'));
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '/frontend/register.html'));
-});
 
 app.get('/gallery', (req, res) => {
     res.sendFile(path.join(__dirname, '/frontend/pages/gallery.html'));
