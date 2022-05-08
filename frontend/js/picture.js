37fdcdf3d32eba9ea2c204f0ddf00e45c2c11041
박태규 <ptq124@naver.com>
2022-05-08 10:42:59 +0900

    update

--- frontend/js/picture.js
+++ frontend/js/picture.js
@@ -1,19 +1,25 @@
 const radio = document.getElementsByTagName('input');
 const imgTag = document.querySelector('.file-home');
 const img = imgTag.getElementsByTagName("img");
+const imgBack = document.querySelector(".file-background");
+const input = document.createElement("input");
+input.setAttribute("type","text");
+input.setAttribute("name","back");
 
 const next = document.querySelector(".nextpage");
 next.addEventListener("click",choseImg);
 
-
 function choseImg(event){
-
+    event.preventDefault();
     for(let i=0; i<radio.length;i++){
         if(radio[i].checked){
-            const imgBack = document.querySelector(".file-background");
             imgBack.appendChild(img[i]);
+            input.setAttribute("value",i);
+            imgBack.appendChild(input);
         }
     }
     document.querySelector(".file-home").classList.add("hidden");
     document.querySelector(".form").classList.remove("hidden");
+    document.querySelector(".container-file").style.height = "140vh";
+    document.querySelector(".file-home").style.height = "calc(150vh - 50vh)";
 }
